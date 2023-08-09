3630ba23915fcd679c29a3daf20be5b9775794e4
Anil Kumar <kumarsark69@gmail.com>
2023-08-09 18:30:29 +0530

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+//Basic example
+//var factory = new ConnectionFactory { HostName = "localhost" };
+
+//using var connection = factory.CreateConnection();
+
+//using var channel = connection.CreateModel();
+//Console.WriteLine("Wating for the Producer");
+//channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+//var consumer = new EventingBasicConsumer(channel);
+//consumer.Received += (model, ea) =>
+//{
+//    var body = ea.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine($"Received message {message}");
+//};
+//channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+//Console.ReadKey();
+
+
+
+//competing consumer
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+var random= new Random();
+using var channel = connection.CreateModel();
+Console.WriteLine("Wating for the Producer");
+channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+
+//This line helps to send the message to the free consumer rather than sending it in round robin format 
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var processingTime = random.Next(1, 6);
+
+    var body = ea.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    
+
+    Console.WriteLine($"Receving message: {message} and it take {processingTime} to process");
+    Task.Delay(TimeSpan.FromSeconds(processingTime)).Wait();
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+};
+channel.BasicConsume(queue: "letterbox", autoAck: false, consumer: consumer);
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//Basic Producer
+//var factory = new ConnectionFactory { HostName = "localhost" };
+
+//using var connection = factory.CreateConnection();
+
+//using var channel = connection.CreateModel();
+
+//channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+//var message = "This is my first message";
+
+//var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+//channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+//Console.WriteLine($"Published message: {message}");
+
+
+//Competing Consumer Producer
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var random = new Random();
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+var messageId = 1;
+channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+while (true)
+{
+    var publishingTime = random.Next(1, 4);
+    var message = $"Sending messageId: {messageId} ";
+
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+    Console.WriteLine($"Published message: {message}");
+
+    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
+
+    messageId++;
+
+
+}
+
+
+
